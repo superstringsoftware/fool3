94f618f5ed9c8112a471d4194ca28250c055ea1a
J Ho XRay <jho.xray@gmail.com>
2017-03-16 11:49:10 +0100

    STARTING SIMPLIFICATION no 1

--- src/Typesystem.hs
+++ src/Typesystem.hs
@@ -13,6 +13,8 @@ module Typesystem where
 import Data.Word
 import qualified Data.Vector.Unboxed as U
 
+import Data.List
+
 type Name = String
 
 -- all available primitive types in the language that we will use as the basis vector to build the type System
@@ -25,36 +27,92 @@ data BuiltinTypeValue = PInt Int | PFloat Float | PByte Word8
                         | VByte (U.Vector Word8)
     deriving (Show, Eq)
 
-data TypeOrKind = Builtin BuiltinType | SELF | TYPE | FUNC | TypeRep SumType deriving Eq
+-- data TypeOrKind = Builtin BuiltinType | SELF | TYPE | FUNC | TypeRep SumType deriving Eq
 
-data ValueRep = PrimValue BuiltinTypeValue deriving Eq
+-- what can be assigned to a type
+data RTypeValue = TYPE | RTypeValPrim BuiltinType | RTypeValSum SumType
+                | RTypeValBound Int deriving (Show, Eq)-- int is an index to which Type Level var we are bound
+-- what can be assigned to value of a var
+data RValue = RValPrim BuiltinTypeValue | RValPrimType BuiltinType | RValSum Constructor | RValBound Int | UNDEFINED deriving (Show, Eq)
 
--- This is where instantiation happens
--- if varType is TYPE - then it's a type variable (a, b in Haskell)
--- if it's TypeRep or Builtin - it's a regular variable
--- if it's FUNC it's a function (Kinds??)
-data RecordField = RecordVariableField {
-    varLabel :: Maybe Name, -- Just name if a record is named, Nothing if anonymous
-    varType :: TypeOrKind
-    -- varValue :: Maybe ValueRep -- if Nothing, variable is unbound, otherwise - concrete value
-} deriving Eq
+-- return type of the primitive value (needed for type comparisons etc)
+checkPrimitiveType :: BuiltinTypeValue -> BuiltinType
+checkPrimitiveType (PInt _) = PrimInt
+checkPrimitiveType (PFloat _) = PrimFloat
+checkPrimitiveType (PByte _) = PrimByte
+checkPrimitiveType (VInt _) = PrimArray PrimInt
+checkPrimitiveType (VFloat _) = PrimArray PrimFloat
+checkPrimitiveType (VByte _) = PrimArray PrimByte
 
--- Product type (including single-terms such as Nothing or True):
-data ProductConstructor = ProductConstructor {
+-- checkRValType :: RValue 
+
+-- checking if RValue is of type RTypeValue
+isValueCorrectType :: RValue -> RTypeValue -> Bool
+-- checking primitives
+isValueCorrectType (RValPrim val) (RTypeValPrim tp) =
+    if (checkPrimitiveType val == tp) then True else False
+isValueCorrectType val TYPE = isValAType val
+                              where isValAType (RValPrimType _) = True
+                                    isValAType (RValSum _) = True
+                                    isValAType _ = False
+
+
+
+-- generic variable that can hold whatever
+data Variable = Variable {
+    varLabel :: Name,
+    varType :: RTypeValue,
+    varValue :: RValue,
+    varDefaultValue :: RValue
+} deriving (Show, Eq)
+
+-- shortcut for Variable initialization
+undefinedVar = Variable {
+    varLabel = "",
+    varType = TYPE,
+    varValue = UNDEFINED,
+    varDefaultValue = UNDEFINED
+}
+
+-- Product type constructor
+data Constructor = Constructor {
     consName :: Name,
-    fields :: [RecordField]
-} deriving Eq
+    consVars :: [Variable], -- Constructor-level variables: Just a --> a etc
+    vars :: [Variable]
+} deriving (Eq)
+
+ptInt = Constructor {
+    consName = "I#",
+    consVars = [],
+    vars = [Variable {
+        varLabel = "",
+        varType = RTypeValPrim PrimInt,
+        varValue = UNDEFINED,
+        varDefaultValue = RValPrim (PInt 0)
+    }]
+}
+
+bindInt i = let vr = (head $ vars ptInt)
+                vn = vr {varValue = RValPrim (PInt i)}
+                in ptInt {vars = [vn]}
+
 
 -- Every concrete Type is a SumType of Products!
 data SumType = SumType {
     typeName :: Name,
-    productTypes :: [ProductConstructor]
+    typeVars :: [Variable], -- list of bound vars in the type declaration
+    constructors :: [Constructor]
 } deriving (Eq)
+
+-- binding type variable in a dependent type - e.g., Maybe Int
+-- returning new type
+-- bindTypeVar :: SumType -> Name -> TypeOrKind -> SumType
+-- bindTypeVar st var tok = let pt = productTypes st
 
 
 {- ********************* Show and other service instances ********************** -}
 instance Show SumType where
-    show st = (typeName st) ++ " = " ++ show (productTypes st)
+    show st = (typeName st) ++ show (typeVars st) ++ " = " ++ show (constructors st)
 
 instance Show BuiltinType where
     show PrimInt = "int "
@@ -63,28 +121,85 @@ instance Show BuiltinType where
     show PrimReference = "reference "
     show (PrimArray t) = "array " ++ (show t)
 
-instance Show TypeOrKind where
-    show (TypeRep st)  = typeName st
-    show SELF = "SELF"
-    show TYPE = "TYPE"
-    show FUNC = "FUNCTION"
-    show (Builtin b) = show b
-
-instance Show RecordField where
-    show rf
-        | varLabel rf == Nothing = "_anonymous_ :: " ++ show (varType rf)
-        | otherwise              = n ++ " :: " ++ show (varType rf) where (Just n) = varLabel rf
-
-instance Show ProductConstructor where
+instance Show Constructor where
     show pc
-        | (fields pc) == [] = (consName pc)
-        | otherwise         = (consName pc) ++ " " ++ show (fmap show (fields pc))
+        | (vars pc) == [] = (consName pc)
+        | otherwise         = (consName pc) ++ " " ++ show (fmap show (vars pc))
 
 
 {- ********************* EXAMPLE TYPES ********************** -}
+
 -- Bool = True | False
 tpBool = SumType {
     typeName = "Bool",
+    typeVars = [],
+    constructors = [
+        Constructor {consName = "True", consVars = [], vars = []},
+        Constructor {consName = "False", consVars = [], vars = []}
+    ]
+}
+
+-- Maybe a = Nothing | Just a
+tpMaybe = SumType {
+    typeName = "Maybe",
+    typeVars = [Variable {varLabel="a", varType = TYPE, varValue=UNDEFINED, varDefaultValue=UNDEFINED}],
+    constructors = [
+        Constructor {consName = "Nothing", consVars = [], vars = []},
+        Constructor {consName = "Just",
+            consVars = [Variable {varLabel="a", varType = TYPE, varValue=RValBound 0, varDefaultValue=UNDEFINED}],
+            vars     = [Variable {varLabel="", varType = RTypeValBound 0, varValue=UNDEFINED, varDefaultValue=UNDEFINED}]
+        }
+    ]
+}
+
+tpDepType = SumType {
+    typeName = "DepTypeExample",
+    typeVars = [undefinedVar {varLabel="a", varType = TYPE},
+                undefinedVar {varLabel="n", varType = RTypeValPrim PrimInt}
+               ],
+    constructors = [
+        Constructor {consName = "DepType",
+            consVars = [undefinedVar {varLabel="a", varType = TYPE, varValue=RValBound 0},
+                        undefinedVar {varLabel="n", varType = RTypeValPrim PrimInt, varValue=RValBound 0}
+                       ],
+            vars     = [undefinedVar {varLabel="name", varType = RTypeValSum tpMaybe}]
+        }
+    ]
+}
+
+-- binds Variable at place Int and returns adjusted Constructor
+-- bindVar :: Variable -> RValue -> Either String Variable
+bindVar var val =
+    if (isValueCorrectType val (varType var))
+        then (Right var{varValue = val})
+        else (Left "ERROR: Type mismatch")
+
+{-
+-- Int - packed int
+tpInt = SumType {
+    typeName = "Int",
+    typeVars = [],
+    productTypes = [
+        ProductConstructor {consName = "I#",
+            fields = [RecordVariableField {varLabel = Nothing, varType = Builtin PrimInt}]}
+    ]
+}
+
+-- Float - packed float
+tpFloat = SumType {
+    typeName = "Float",
+    typeVars = [],
+    productTypes = [
+        ProductConstructor {consName = "F#",
+            fields = [RecordVariableField {varLabel = Nothing, varType = Builtin PrimFloat}]}
+    ]
+}
+
+
+-- Bool = True | False
+tpBool = SumType {
+    typeName = "Bool",
+    typeVars = [],
     productTypes = [
         ProductConstructor {consName = "True", fields = []},
         ProductConstructor {consName = "False", fields = []}
@@ -94,6 +209,7 @@ tpBool = SumType {
 -- Point {x,y:int}, Circle, Square polymorphism
 tpPoint = SumType {
     typeName = "Point",
+    typeVars = [],
     productTypes = [
         ProductConstructor {consName = "Point",
             fields = [RecordVariableField {varLabel = Just "x", varType = Builtin PrimInt},
@@ -105,6 +221,7 @@ tpPoint = SumType {
 -- Circle c; c.x = 5 -- accessing Point fields directly!
 tpCircle = SumType {
     typeName = "Circle",
+    typeVars = [],
     productTypes = [
         ProductConstructor {consName = "Circle",
             fields = [RecordVariableField {varLabel = Nothing, varType = TypeRep tpPoint}, -- anonymous field, means can access Point fields directly!!!
@@ -116,6 +233,7 @@ tpCircle = SumType {
 -- Circle c; c.r = 5, c.p.x = 5 -- accessing Point fields via p:Points!
 tpCircle1 = SumType {
     typeName = "Circle1",
+    typeVars = [],
     productTypes = [
         ProductConstructor {consName = "Circle1",
             fields = [RecordVariableField {varLabel = Just "p", varType = TypeRep tpPoint},
@@ -126,6 +244,7 @@ tpCircle1 = SumType {
 -- Maybe a = Nothing | Just a
 tpMaybe = SumType {
     typeName = "Maybe",
+    typeVars = ["a"],
     productTypes = [
         ProductConstructor {consName = "Nothing", fields = []},
         ProductConstructor {consName = "Just",
@@ -133,9 +252,24 @@ tpMaybe = SumType {
     ]
 }
 
+-- Either a b = Left a | Right b
+tpEither = SumType {
+    typeName = "Either",
+    typeVars = ["a", "b"],
+    productTypes = [
+        ProductConstructor {consName = "Left",
+            fields = [RecordVariableField {varLabel = Just "a", varType = TYPE}]},
+        ProductConstructor {consName = "Right",
+            fields = [RecordVariableField {varLabel = Just "b", varType = TYPE}]}
+    ]
+}
+
+
+
 -- Maybe Int - exploring difference between concrete and parametric
 tpMaybeInt = SumType {
     typeName = "Maybe Int",
+    typeVars = [],
     productTypes = [
         ProductConstructor {consName = "Nothing", fields = []},
         ProductConstructor {consName = "Just",
@@ -147,6 +281,7 @@ tpMaybeInt = SumType {
 -- then, can do things such as Vector Float 2 or Vector Float 4 !
 tpVector = SumType {
     typeName = "Vector",
+    typeVars = ["a", "n"],
     productTypes = [
         ProductConstructor {consName = "Vector",
             fields = [RecordVariableField {varLabel = Just "a", varType = TYPE},
@@ -155,20 +290,11 @@ tpVector = SumType {
             ]
         }
 
--- how about Vector Float 4? -- can't do it yet!
-tpVectorF4 = SumType {
-    typeName = "Vector Float 4",
-    productTypes = [
-        ProductConstructor {consName = "Vector",
-            fields = [RecordVariableField {varLabel = Nothing, varType = Builtin PrimFloat},
-                      RecordVariableField {varLabel = Nothing, varType = Builtin PrimInt}]
-                  }
-            ]
-        }
 
 -- List a = Nil | Cons a (List a) - recursive datatype, tricky!
 tpList = SumType {
     typeName = "List",
+    typeVars = ["a"],
     productTypes = [
         ProductConstructor {consName = "Nil", fields = []},
         ProductConstructor {consName = "Cons",
@@ -176,3 +302,14 @@ tpList = SumType {
                       RecordVariableField {varLabel = Nothing, varType = SELF}]}
     ]
 }
+
+tpS = SumType {
+    typeName = "S",
+    typeVars = ["a"],
+    productTypes = [
+        ProductConstructor {consName = "S",
+            fields = [RecordVariableField {varLabel = Just "a", varType = TYPE},
+                      RecordVariableField {varLabel = Nothing, varType = SELF}]}
+    ]
+}
+-}
