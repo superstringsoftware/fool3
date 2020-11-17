a7d2360c411025219a1c52afd49ee12b6b9050aa
Jay X-Ray Ho <jho.xray@gmail.com>
2020-11-17 19:35:12 +0100

    Parser major clean up done

--- lambda/src/SurfaceLanguage/Lambda/Parser.hs
+++ lambda/src/SurfaceLanguage/Lambda/Parser.hs
@@ -87,7 +87,7 @@ pPreds = do
 pPred :: Parser Pred
 pPred = do
     tcon <- (TCon <$> uIdentifier) <?> "type class name (should start from the upper case)." 
-    vars <- many1 $ (TVar <$> variable)                
+    vars <- many1 $ (TVar <$> pVar)                
     return $ Exists $ TApp tcon vars -- foldl TApp tcon vars -- type application
 
 -- Next, top level identifier: name and optional type signature
@@ -144,7 +144,7 @@ pTopLevelPatternMatch = do
     --dbg "pPatternMatch started"
     h <- pArgs <?> "pArgs in pattern match"
     reservedOp "="
-    t <- expr
+    t <- pExpr
     --dbg "pPatternMatch ended"
     return $ PatternMatch [h] t
 
@@ -163,7 +163,7 @@ pContainers = -- try  (FlTuple TTVector <$> angles   (commaSep expr)) <|>
         <|> (try pFields >>= \args -> return (Rec args))
         -- <|> try (braces (commaSep pAnonVar) >>= \args -> return $ Rec $ vars2record args)
         <|> try (braces (commaSep pExpr) >>= \args -> return $ Rec $ recordFromExprs args)
-        <|> (angles (commaSep factor)  >>= \args -> return (Lit $ LVec args))
+        <|> (angles (commaSep pFactor)  >>= \args -> return (Lit $ LVec args))
 
 pArg :: Parser Expr
 pArg = try pContainers
@@ -244,10 +244,6 @@ binops = [[binary "=" Ex.AssocLeft]
 -- helper parsers: lower case and upper case
 lIdentifier = skipMany space >> lookAhead lower >> identifier
 uIdentifier = skipMany space >> lookAhead upper >> identifier
-    
-expr :: Parser Expr
-expr = Ex.buildExpressionParser (binops ++ [[unop],[binop]] ++ [[binary "==" Ex.AssocLeft]] ) factor
--- expr = Ex.buildExpressionParser ([]) factor
 
 ---------------------------------------------------------------------------------------------------
 -- Parsing typed identifier
@@ -255,8 +251,8 @@ expr = Ex.buildExpressionParser (binops ++ [[unop],[binop]] ++ [[binary "==" Ex.
 -- concrete type or type application
 typeAp :: Parser Type
 typeAp = do
-  tcon <- try concreteType <|> (TVar <$> variable)
-  vars <- many $ try (TCon <$> uIdentifier) <|> try ( TVar <$> variable ) 
+  tcon <- try concreteType <|> (TVar <$> pVar)
+  vars <- many $ try (TCon <$> uIdentifier) <|> try ( TVar <$> pVar ) 
                  <|> (parens typeAp) -- <|> (TExpr <$> argument)
   if null vars then return tcon -- concrete type
   else return $ TApp tcon vars -- foldl TApp tcon vars -- type application
@@ -294,25 +290,7 @@ typeVar = do
   name <- lIdentifier
   return $ TVar $ Var name SmallType
 
--- variable with type (or any identified for that matter, including function definitions)
--- including operators
-variable :: Parser Var
-variable = do
-  name <- try (parens op) <|> identifier --emptyStringParser -- added unnamed variables for easier record parsing
-  typ <- typeSignature
-  return $ Var name typ
 
-anonVariable :: Parser Var
-anonVariable = do
-    typ <- strictTypeSignature
-    return $ Var "" typ
-  
-
-typedId :: Parser Expr
-typedId = do
-  name <- try (parens op) <|> identifier --emptyStringParser -- added unnamed variables for easier record parsing
-  typ <- strictTypeSignature
-  return $ Let $ Field name typ EMPTY
 ---------------------------------------------------------------------------------------------------
 int :: Parser Literal
 int = LInt . fromInteger <$> integer
@@ -330,6 +308,7 @@ symbolId = do
 
 -- everything is a function
 -- this is a huge and entangled method doing a lot, needs REFACTORING and SIMPLIFICATION!!!
+{-
 lambda :: Parser Expr
 lambda = do
     pos <- getPosition
@@ -363,25 +342,6 @@ lambda = do
                 return $ Let $ Field name tp ex
         _           -> return $ Let $ Field name tp ex -- top level function, no "in" for LET
 
-predicate :: Parser Pred
-predicate = do
-    tcon <- (TCon <$> uIdentifier) <?> "type class name" 
-    vars <- many1 $ (TVar <$> variable)                
-    return $ Exists $ TApp tcon vars -- foldl TApp tcon vars -- type application
-
-predicates :: Parser [Pred]
-predicates = do
-    try (reserved "exists") <|> reserved "∃"
-    preds <- try (parens (sepBy1 predicate (reservedOp ",") )) <|> (predicate >>= \p -> pure [p])
-    reservedOp "=>"
-    return preds
-
-typeclass :: Parser Expr    
-typeclass = do
-    setInsideTypeclass True
-    s <- braces (sepBy1 defn (reservedOp ";")) <?> "error while parsing typeclass"
-    setInsideTypeclass False
-    return $ Tuple "" s ToDerive
 
 -- pattern match inside a function    
 lambdaPatternMatch :: Parser Expr
@@ -399,85 +359,8 @@ lambdaPatternMatch = do
                                          (sourceColumn pos) "" 
                                          (("function " ++ L.unpack nm ++  " has arity " ++ show a ++ " but pattern match has " ++ show (length h) ++ " arguments"))
             return $ PatternMatch h t
+-}
 
--- Tuple that data constructors return, can only contain names or "_" symbols    
-consTuple :: Parser Expr
-consTuple = do
-    pos <- getPosition
-    a <- getCurrentArity
-    args <- braces (many symbolId) 
-    if ( (length args) == a ) then return (Tuple "" args ToDerive)
-    else do
-        nm <- getCurrentLambdaName
-        lift $ logError $ LogPayload (sourceLine pos) 
-                    (sourceColumn pos) "" 
-                    (("constructor " ++ L.unpack nm ++  " has arity " ++ show a 
-                             ++ " but it's tuple has " ++ show (length args) ++ " arguments"))
-        return (Tuple "" args ToDerive)
-
-
-argumentsOnly :: Parser [Expr]
-argumentsOnly = do
-    args <- many1 argument
-    return args
-    
-
--- function with pattern matches defined inside
-{-
-length:Int = \ ls:(List a) . {
-    Nil -> 0;
-    (Cons _ xs) -> 1 + length xs
-};
--}    
-patternsInsideLambda :: Parser Expr
-patternsInsideLambda = do
-    s <- braces (sepBy1 lambdaPatternMatch (reservedOp ";")) <?> "error while parsing patterns inside lambda"
-    return $ Patterns s
-
-
--- top-level pattern match, like in Haskell
-patternMatch :: Parser Expr
-patternMatch = do
-    h <- arguments
-    reservedOp "="
-    t <- expr
-    return $ PatternMatch [h] t
-
-
--- simple top-level binding of a symbol to expression, without lambdas
-binding :: Parser Expr
-binding = do
-    var@(Var name tp) <- variable -- identifier
-    reservedOp "="
-    let bodyAction = if (tp /= SmallType) then expr else expr
-    body <- bodyAction
-    return $ Let $ Field name tp body -- top level function, no "in" for LET
-
-
-argument :: Parser Expr
-argument = try containers
-    <|> try (parens expr)
-    <|> try typedId
-    <|> try (Lit <$> floating)
-    <|> try (Lit <$> int)
-    <|> try (Lit <$> stringVal)
-    <|> symbolId
-    <?> "container, literal, symbol id or parenthesized expression"
-
-arguments :: Parser Expr
-arguments = do
-    args@(f:xs) <- many1 argument
-    --return $ foldl1 App args -- need to use left fold here because application is highest precedence
-    let er = if (xs == []) then f else (App f xs)
-    return er
-  
-containers :: Parser Expr
-containers = -- try  (FlTuple TTVector <$> angles   (commaSep expr)) <|>
-        try  $ do
-            args <- brackets (commaSep expr)
-            return $ Lit $ LList args
-        <|> try (braces (commaSep expr) >>= \args -> return (Tuple "" args ToDerive))
-        <|> (angles (commaSep factor)  >>= \args -> return (Lit $ LVec args))
 
 -- contents :: Parser a -> Parser a
 contents p = do
@@ -486,34 +369,9 @@ contents p = do
     eof
     return r
 
-factor :: Parser Expr
--- factor = try caseExpression <|> try call <|> try ifthen <|> argument -- arguments -- try letins <|>
-factor = try patternsInsideLambda <|> try typeclass <|> try consTuple <|> arguments -- try caseExpression <|> arguments -- arguments -- try letins <|>
-
-defn :: Parser Expr
-defn =  do
-    expr <- try lambda
-            -- <|> try anonConstructor
-            <|> try binding
-            <|> try patternMatch
-            <|> (VarDefinition <$> variable)
-            <?> "lambda, binding, pattern match or expression"
-    return expr
-        
-        
-
-toplevel :: Parser [Expr]
-toplevel = many $ do
-    pos <- getPosition
-    def <- defn
-    reservedOp ";"
-    ints <- lift get 
-    let pm = (def, SourceInfo (sourceLine pos) (sourceColumn pos) ""):(parsedModule ints)
-    lift $ put ints {parsedModule = pm}    
-    return def
     
 -- parseExpr :: String -> Either ParseError Expr
-parseExpr s = runParserT (contents expr) initialParserState "<stdin>" s
+parseExpr s = runParserT (contents pExpr) initialParserState "<stdin>" s
 
 --parseToplevel :: String -> Either ParseError [Expr]
 -- parseToplevel s = runParserT (contents defn) initialParserState "<stdin>" s
@@ -523,7 +381,7 @@ parseExpr s = runParserT (contents expr) initialParserState "<stdin>" s
 
 -- parse a given file
 parseToplevelFile :: String -> IntState (Either ParseError [Expr])
-parseToplevelFile name = parseFromFile (contents toplevel) name initialParserState
+parseToplevelFile name = parseFromFile (contents pToplevel) name initialParserState
 
 parseFromFile :: Parser a -> String -> ParserState -> IntState (Either ParseError a)
 -- redefining parse from file to work with our state - just a quick and dirty fix
